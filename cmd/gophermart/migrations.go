6160a85ab37be4362dd89ba354850fdd3f34da40
user <volokitina.anna@gmail.com>
2022-04-16 17:59:20 +0300

    autorization

--- cmd/gophermart/migrations.go
+++ cmd/gophermart/migrations.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"fmt"
-	"log"
-
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
-const pathToMigrations = "file://../../migrations"
+const pathToMigrations = "file://migrations"
 
 func doMigrates(dsn string) {
 	m, err := migrate.New(pathToMigrations, dsn)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
 	if err := m.Up(); err != nil {
 		fmt.Println(err)

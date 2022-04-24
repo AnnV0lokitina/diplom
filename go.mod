9a12f8284379be5c741bbcab1b9d1d558f1fd8cf
user <volokitina.anna@gmail.com>
2022-04-24 22:52:41 +0300

    fix

--- go.mod
+++ go.mod
@@ -17,10 +17,12 @@ require (
 	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.10.0 // indirect
+	github.com/joeljunstrom/go-luhn v0.0.0-20190413165225-1e071b33b576 // indirect
 	github.com/lib/pq v1.10.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/stretchr/testify v1.7.1 // indirect
+	github.com/theplant/luhn v0.0.0-20170224032821-81a1a381387a // indirect
 	golang.org/x/crypto v0.0.0-20220210151621-f4118a5b28e2 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
--- internal/entity/order_number.go
+++ internal/entity/order_number.go
@@ -1,47 +1,11 @@
 package entity
 
 import (
-	"errors"
+	"github.com/joeljunstrom/go-luhn"
 )
-
-const code0 = 48
 
 type OrderNumber string
 
 func (on OrderNumber) Valid() bool {
-	valid, err := checkLuhn(on)
-	if err != nil {
-		return false
-	}
-	return valid
-}
-
-func byteToDigit(b byte) (int, error) {
-	n := b - code0
-	if n <= 9 {
-		return int(n), nil
-	}
-	return 0, errors.New("illegal order number")
-}
-
-func checkLuhn(cardNumber OrderNumber) (bool, error) {
-	sum := 0
-	length := len(cardNumber)
-	for i := 0; i < length; i++ {
-		number, err := byteToDigit(cardNumber[i])
-		if err != nil {
-			return false, err
-		}
-		var val int
-		if i%2 == 0 {
-			val = number
-		} else {
-			val = number * 2
-			if val > 9 {
-				val -= 9
-			}
-		}
-		sum += val
-	}
-	return sum%10 == 0, nil
+	return luhn.Valid(string(on))
 }
